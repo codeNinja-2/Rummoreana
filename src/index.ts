64795162b1ec20ca071305ab11b53f4ff2c5f9cb
codeNinja-2 <74163134+codeNinja-2@users.noreply.github.com>
2025-08-10 20:56:31 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello");
+enum Suit {
+    Spades,
+    Diamonds,
+    Hearts,
+    Clubs
+}
+
+enum Rank {
+    Ace,
+    Number2,
+    Number3,
+    Number4,
+    Number5,
+    Number6,
+    Number7,
+    Number8,
+    Number9,
+    Number10,
+    Jack,
+    Queen,
+    King
+
+}
+
+class Card {
+    constructor(readonly suit: Suit) {
+    }
+}
