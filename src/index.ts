d8a517c28e630ae7b6a10eafadff2231647c6983
codeNinja-2 <74163134+codeNinja-2@users.noreply.github.com>
2025-08-11 09:58:07 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,41 +6,32 @@ enum Suit {
 }
 
 enum Rank {
-    Ace,
-    Number2,
-    Number3,
-    Number4,
-    Number5,
-    Number6,
-    Number7,
-    Number8,
-    Number9,
-    Number10,
-    Jack,
-    Queen,
-    King
+    Ace = 1,
+    Number2 = 2,
+    Number3 = 3,
+    Number4 = 4,
+    Number5 = 5,
+    Number6 = 6,
+    Number7 = 7,
+    Number8 = 8,
+    Number9 = 9,
+    Number10 = 10,
+    Jack = 11,
+    Queen = 12,
+    King = 13
 }
-
-const cardValues = new Map<Rank, number>()
-cardValues.set(Rank.Ace, 15)
-cardValues.set(Rank.Number2, 15)
-cardValues.set(Rank.Number3, 5)
-cardValues.set(Rank.Number4, 5)
-cardValues.set(Rank.Number5, 5)
-cardValues.set(Rank.Number6, 5)
-cardValues.set(Rank.Number7, 5)
-cardValues.set(Rank.Number8, 5)
-cardValues.set(Rank.Number9, 5)
-cardValues.set(Rank.Number10, 10)
-cardValues.set(Rank.Jack, 15)
-cardValues.set(Rank.Queen, 15)
-cardValues.set(Rank.King, 15)
 
 class Card {
     constructor(readonly suit: Suit, readonly rank: Rank) {
     }
 
     getValue(): number {
-        return cardValues.get(this.rank)
+        if (this.rank == Rank.Ace) {
+            return 15;
+        } else if (this.rank >= 11) {
+            return 10;
+        } else {
+            return 5;
+        }
     }
 }
